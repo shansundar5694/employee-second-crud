1bb6ed8017d1c64dd89726f9a9102b48872a1b8c
shansundar <shansundar345@gmail.com>
2025-01-24 13:07:26 +0530

    user-forgot-backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./Database/dbconfig.js";
-import empRouter from "./Routers/employee.router.js";
+import userRouter from "./Routers/user.router.js";
 
 dotenv.config();
 const app = express();
@@ -10,11 +10,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(cors());
-app.use(express.json()); // for postcall data whta we send is not json.. so.. we include express.json
+app.use(express.json());
 
 connectDB();
 
-app.use("/api", empRouter);
+app.use("/api/user", userRouter);
 
 app.listen(port, () => {
   console.log("app is running on", port);
